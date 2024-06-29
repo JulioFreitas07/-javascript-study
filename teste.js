949b9b1b005b723f0bc46ddab64024d376579ed5
JulioFreitas07 <freitas27j@gmail.com>
2024-06-29 00:11:55 -0300

    changes

---
+++ teste.js
@@ -0,0 +1,13 @@
+var preco =  3; //pode ser alterada
+preco =+ 1;
+const PRECO = 9; // não pode ser alterada
+
+function soma(a,b)
+{
+    return a + b;
+}
+
+var c = soma(2, 6);
+
+console.log(c);
+
