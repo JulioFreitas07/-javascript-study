344b5d01c7c9aafdee1f73692186feece74553b3
JulioFreitas07 <freitas27j@gmail.com>
2024-07-03 19:40:06 -0300

    my first addEventListener usage, and little bit about css to change visibility

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,11 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0; 
 
+var message = document.getElementById('message');
+var button = document.getElementById('button');
+button.addEventListener('click', ()=>{message.classList.toggle('reveal');})
+
+
 function increment()
 {
     currentNumber++;
