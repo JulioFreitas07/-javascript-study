b8a622d63977ccc56ae23dd7307ad77109c9eeb5
JulioFreitas07 <freitas27j@gmail.com>
2024-07-03 18:45:45 -0300

    introdution to DOM, div, p, span and my first javascript method

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,14 @@
-console.log('HelloWord')
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0; 
+
+function increment()
+{
+    currentNumber++;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement()
+{
+    currentNumber--;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
